6205bf61012a6d8a6d78118c3f2c965e217b355d
pesala.shravan@skillassure.com <pesala.shravan@skillassure.com>
2022-08-02 10:26:35 +0000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDCi30OyyVhtlLcZklfde2fpnJxe3UZTqs",
+    authDomain: "connect-edd11.firebaseapp.com",
+    projectId: "connect-edd11",
+    storageBucket: "connect-edd11.appspot.com",
+    messagingSenderId: "736715696667",
+    appId: "1:736715696667:web:292e3a3773ce27015926be"
+  }
 };
